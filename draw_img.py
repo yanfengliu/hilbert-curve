7479b09f58102b16fc05ff9a7fa5101a3c71590c
Yanfeng Liu <yanfengliux@gmail.com>
2024-05-27 15:27:56 -0700

    Recursively draw hilbert curve

--- draw_img.py
+++ draw_img.py
@@ -7,61 +7,72 @@ curve_width = 3
 half_curve_width = (curve_width - 1) // 2
 square_width = 15
 first_order_length = 3 * border_width + 2 * square_width
-
-canvas = np.zeros((first_order_length, first_order_length, 3)) + 1
-
-# Draw the border
-canvas[0, :] = 0
-canvas[:, 0] = 0
-canvas[-1, :] = 0
-canvas[:, -1] = 0
-mid = border_width + square_width
-canvas[mid, :] = 0
-canvas[:, mid] = 0
-
-# Draw the hilbert curve
-
-# Draw first order.
 x = int(border_width + 0.5 * (square_width - 1))
-canvas[x:-x, (x - half_curve_width) : (x + half_curve_width + 1)] = line_color
-canvas[
-    (x - half_curve_width) : (x + half_curve_width + 1),
-    (x - half_curve_width) : (-x + half_curve_width),
-] = line_color
-canvas[x:-x, (-(x + 1) - half_curve_width) : (-(x + 1) + half_curve_width + 1)] = (
-    line_color
-)
+keep_grid = False
+
+
+def get_1st_order_hc():
+    canvas = np.zeros((first_order_length, first_order_length, 3)) + 1
+
+    # Draw the grid
+    if keep_grid:
+        mid = border_width + square_width
+        canvas[mid, :] = 0
+        canvas[:, mid] = 0
+
+    # Draw first order.
+    canvas[x:-x, (x - half_curve_width) : (x + half_curve_width + 1)] = line_color
+    canvas[
+        (x - half_curve_width) : (x + half_curve_width + 1),
+        (x - half_curve_width) : (-x + half_curve_width),
+    ] = line_color
+    canvas[x:-x, (-(x + 1) - half_curve_width) : (-(x + 1) + half_curve_width + 1)] = (
+        line_color
+    )
+    return canvas
+
+
+def recursive_hc(order: int):
+    if order < 1 or not isinstance(order, int):
+        raise ValueError("Order must be a positive integer.")
+    if order == 1:
+        return get_1st_order_hc()
+    else:
+        prev_order = recursive_hc(order - 1)
+        length = prev_order.shape[0]
+        full_length = 2 * length - border_width
+        img = np.zeros((full_length, full_length, 3)) + 1
+        img[:length, :length] = prev_order
+        img[:length:, -length:] = prev_order
+        img[-length:, :length] = np.rot90(prev_order, -1)
+        img[-length:, -length:] = np.rot90(prev_order, 1)
+
+        # Connect the previous order segments.
+        img[
+            (length - x - half_curve_width) : (length + x + half_curve_width),
+            (x - half_curve_width) : (x + half_curve_width + 1),
+        ] = line_color
+        img[
+            (length - x - half_curve_width) : (length + x + half_curve_width),
+            (-(x + 1) - half_curve_width) : (-(x + 1) + half_curve_width + 1),
+        ] = line_color
+        img[
+            ((length - x - 1) - half_curve_width) : ((length - x) + half_curve_width),
+            (length - x - half_curve_width - 1) : (length + x + half_curve_width),
+        ] = line_color
+
+        return img
+
 
 # Draw second order.
-full_length = 2 * first_order_length - border_width
-second_order = np.zeros((full_length, full_length, 3)) + 1
-second_order[:first_order_length, :first_order_length] = canvas
-second_order[:first_order_length:, -first_order_length:] = canvas
-second_order[-first_order_length:, :first_order_length] = np.rot90(canvas, -1)
-second_order[-first_order_length:, -first_order_length:] = np.rot90(canvas, 1)
+final_img = recursive_hc(4)
 
-# Connect the first order segments.
-second_order[
-    (first_order_length - x - half_curve_width) : (
-        first_order_length + x + half_curve_width
-    ),
-    (x - half_curve_width) : (x + half_curve_width + 1),
-] = line_color
-second_order[
-    (first_order_length - x - half_curve_width) : (
-        first_order_length + x + half_curve_width
-    ),
-    (-(x + 1) - half_curve_width) : (-(x + 1) + half_curve_width + 1),
-] = line_color
-second_order[
-    ((first_order_length - x - 1) - half_curve_width) : (
-        (first_order_length - x) + half_curve_width
-    ),
-    (first_order_length - x - half_curve_width - 1) : (
-        first_order_length + x + half_curve_width
-    ),
-] = line_color
+# Draw the border
+final_img[0, :] = 0
+final_img[:, 0] = 0
+final_img[-1, :] = 0
+final_img[:, -1] = 0
 
 # Show canvas
-plt.imshow(second_order)
+plt.imshow(final_img)
 plt.show()
