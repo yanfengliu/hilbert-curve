5739274d7e8fe2074f4fbaa71e2956ba2c53c4f3
Yanfeng Liu <yanfengliux@gmail.com>
2024-05-27 14:55:16 -0700

    Support RGB color on the curve

--- draw_img.py
+++ draw_img.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+line_color = np.array([36, 226, 255]) / 255
 line_width = 1
 square_width = 5
 first_order_length = 3 * line_width + 2 * square_width
 
-canvas = np.zeros((first_order_length, first_order_length)) + 255
+canvas = np.zeros((first_order_length, first_order_length, 3)) + 1
 
 # Draw the border
 canvas[0, :] = 0
@@ -20,24 +21,24 @@ canvas[:, mid] = 0
 
 # Draw first order.
 x = int(line_width + 0.5 * (square_width - 1))
-canvas[x:-x, x] = 0
-canvas[x, x:-x] = 0
-canvas[x:-x, -(x + 1)] = 0
+canvas[x:-x, x] = line_color
+canvas[x, x:-x] = line_color
+canvas[x:-x, -(x + 1)] = line_color
 
 # Draw second order.
 full_length = 2 * first_order_length - line_width
-second_order = np.zeros((full_length, full_length)) + 255
+second_order = np.zeros((full_length, full_length, 3)) + 1
 second_order[:first_order_length, :first_order_length] = canvas
 second_order[:first_order_length:, -first_order_length:] = canvas
 second_order[-first_order_length:, :first_order_length] = np.rot90(canvas, -1)
 second_order[-first_order_length:, -first_order_length:] = np.rot90(canvas, 1)
 
 # Connect the first order segments.
-second_order[first_order_length - x : first_order_length + x, x] = 0
-second_order[first_order_length - x : first_order_length + x, -(x + 1)] = 0
+second_order[first_order_length - x : first_order_length + x, x] = line_color
+second_order[first_order_length - x : first_order_length + x, -(x + 1)] = line_color
 second_order[
     (first_order_length - x - 1), first_order_length - x : first_order_length + x
-] = 0
+] = line_color
 
 # Show canvas
 plt.imshow(second_order, cmap="gray")
